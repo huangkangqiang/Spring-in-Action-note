4915c97e0ccc29c1b2d032d8f964e6dc15be595f
huangkangqiang <243286916@qq.com>
2017-06-04 19:23:37 +0800

    更正代码错误

--- Chapter_01/sia.knights/src/main/java/sia/knights/BraveKnight.java
+++ Chapter_01/sia.knights/src/main/java/sia/knights/BraveKnight.java
@@ -3,18 +3,14 @@ package sia.knights;
 public class BraveKnight implements Knight {
 
 	private Quest quest;
-	private Minstrel minstrel;
 
-	public BraveKnight(Quest quest, Minstrel minstrel) {// Quest注入
+	public BraveKnight(Quest quest) {// Quest注入
 		this.quest = quest;
-		this.minstrel = minstrel;
 	}
 
 	@Override
 	public void embarOnQuest() {
-		minstrel.singBeforeQuest();// 骑士需要管理吟游诗人吗？
 		quest.embark();
-		minstrel.singAfterQuest();
 	}
 
 }
